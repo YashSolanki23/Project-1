ab404b6c57bc91e093038b66180a54a7a11a2a69
YashSolanki23 <YashSolanki23@proton.me>
2025-12-14 20:33:08 +0530

    added auth

---
+++ tests/auth.test.ts
@@ -0,0 +1,76 @@
+import request from "supertest"
+import {describe,it,beforeAll, expect} from "vitest"
+import { createApp } from "../src/app"
+import e from "express";
+
+
+const app=createApp();
+
+const user = {
+  email: `test_${Date.now()}@example.com`,
+  password: "12345678",
+};
+
+
+let refreshToken:string;
+
+
+describe("Auth Flow",()=>{
+// registration of user
+  it("Register a new user",async ()=>{
+    const result=await request(app)
+    .post("/auth/register")
+    .send(user);
+ 
+  expect(result.status).toBe(201);
+  expect(result.body).toHaveProperty("id");
+  expect(result.body.email).toBe(user.email);
+ });
+// logs in user
+it("logins in a user",async ()=>{
+const result=await request(app)
+.post("/auth/login")
+.send(user);
+
+expect(result.status).toBe(200);
+expect(result.body).toHaveProperty("accessToken");
+expect(result.body).toHaveProperty("refreshToken");
+
+refreshToken=result.body.refreshToken;
+});
+// refreshes access token
+it("refreshes access token",async ()=>{
+  const result=await request(app)
+  .post("/auth/refresh")
+  .send({refreshToken});
+
+  expect(result.status).toBe(200);
+  expect(result.body).toHaveProperty("accessToken");
+})
+
+
+// login with wrong password
+it("fails a login with wrong password",async ()=>{
+const result=await request(app)
+.post("/auth/login")
+.send({
+  email:user.email,
+  password:"wrong password"
+});
+
+expect(result.status).toBe(401);
+});
+
+
+//wrong refresh token
+it("fails refresh with invalid token",async ()=>{
+const result=await request(app)
+.post("/auth/refresh")
+.send({ refreshToken:"fake.refresh.token"});
+
+expect(result.status).toBe(401);
+
+});
+
+
+});
